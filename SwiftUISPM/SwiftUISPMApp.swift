30d7cae6bbd72f676f2755b5a5bfd0e1244ac8e7
Maulik shah <maulik_shah@hotmail.co.in>
2025-02-16 20:04:09 +0530

    First Init

---
+++ SwiftUISPM/SwiftUISPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISPMApp.swift
+//  SwiftUISPM
+//
+//  Created by Maulik Shah on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
